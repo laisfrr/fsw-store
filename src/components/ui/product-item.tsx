eeec8e4aeef87f154178d755b9cb6ffc817dec7e
laisfrr <laisf.oliv@gmail.com>
2023-10-21 21:05:38 -0300

    fix: ensure product price are wraped

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -9,8 +9,8 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className=" relative flex h-[170px] w-[156px]  items-center justify-center rounded-lg bg-accent">
+    <div className="flex max-w-[170px] flex-col gap-4">
+      <div className=" relative flex h-[170px] w-[170px]  items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -21,7 +21,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
           alt={product.name}
         />
         {product.discountPercentage > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+          <Badge className="absolute left-2 top-2 px-2 py-[2px]">
             <ArrowDown size={14} />
             {product.discountPercentage}%
           </Badge>
@@ -34,15 +34,15 @@ const ProductItem = ({ product }: ProductItemProps) => {
         <div className="flex items-center gap-2">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
-              <p className=" text-xs line-through opacity-75">
+              <p className=" overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="text-sm font-semibold">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
               R$ {product.totalPrice.toFixed(2)}
             </p>
           )}
